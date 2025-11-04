1b0f733807438ba0b4ae1c5dd5b3ff44bc1b25ac
Eglantine2610 <eglantine.arcis.stcharles@gmail.com>
2025-11-04 08:32:44 +0000

    feat: Maîtrise des variables et conditions

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 console.log("Laboratoire prêt !");
+const nom = "Eglantine";
+let age = 17;
+//nom = "ARCIS"; 
+// Uncaught TypeError: Assignment to constant variable.
+
+age = 18
+console.log(age);
+
+const motDePasseAttendu = "secret123";
+let motDePasseutilisateur ="12345"; // Changez cette valeur pour tester
+motDePasseUtilisateur = "secret123";
+if (motDePasseUtilisateur === motDePasseAttendu) {
+    console.log("Accès autorisé. Bienvenue !");
+} else if (motDePasseUtilisateur.length< 8) {
+    console.log("Accès refusé. Votre mot de passe est trop court (8 caractères min).");
+} else {
+    console.log("Accès refusé. Mot de passe incorrect.");
+} 
+
+
